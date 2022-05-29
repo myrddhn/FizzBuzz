a29bfeed7bb3823fdb38ddfd2b85624faa0eab04
Andi Reinbrech <andi@darwinistic.com>
2022-05-29 17:21:19 +0200

    Add project files.

---
+++ FizzBuzz/FBGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    class FBGame
+    {
+        public List<String> fbList{ get; }
+
+        public FBGame()
+        {
+            fbList = new List<string>();
+            for (int n = 1; n <= 20; n++)
+                fbList.Add(((n % 3 == 0) && (n % 5 == 0)) ? "FizzBuzz" : (n % 3 == 0) ? "Fizz" : (n % 5 == 0 ?  "Buzz" : n.ToString()));
+        }
+    }
+}
---
+++ FizzBuzz/FBGameTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class FBGameTest
+    {
+        static void Main(string[] args)
+        {
+            foreach (String s in new FBGame().fbList)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
